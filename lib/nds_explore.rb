26fc71b2f39e7283c3d063bed74c103bc6a9eada
Will Drougas <wdrougas@gmail.com>
2019-10-16 16:51:36 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -11,5 +11,8 @@ def print_first_directors_movie_titles
   i = 0
   movies = directors_database[0][:movies]
   while i < movies.count do
-    put 
+    put movies[i]
+    i += 1
+  end
+  directors_database
 end
